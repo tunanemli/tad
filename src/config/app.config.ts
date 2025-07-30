e5f298a58535ad0e8fa330ad1d8d1f038c127a40
tunanemli <tunanemli79@gmail.com>
2025-07-31 00:36:10 +0300

    base url değisti

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -7,5 +7,5 @@ export interface AppConfig {
 export const appConfig = (): AppConfig => ({
   clientId: process.env.CLIENT_ID || 'LOGO',
   clientSecret: process.env.CLIENT_SECRET || 'Logo123456',
-  baseUrl: process.env.BASE_URL || 'https://api.logo.com.tr',
+  baseUrl: process.env.BASE_URL || 'https://api.logo.com.tr/api/v1',
 });
